be069c6d9ddfd21ddbf62cd9297390129694b88a
Fresh-vegetable-chook <3220582130@qq.com>
2021-08-24 17:39:14 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+//实现计算器
+
+void menu() {
+	printf("*****************************************************\n");
+	printf("*******************    1. add    ********************\n");
+	printf("*******************    2. sub    ********************\n");
+	printf("*******************    3. mul    ********************\n");
+	printf("*******************    4. div    ********************\n");
+	printf("*******************    5. exp    ********************\n");
+	printf("*******************    6. xor    ********************\n");
+	printf("*******************    0. exit   ********************\n");
+	printf("*****************************************************\n");
+}
+
+int Add(int x, int y) {
+	return x + y;
+}
+
+int Sub(int x, int y) {
+	return x - y;
+}
+
+int Mul(int x, int y) {
+	return x * y;
+}
+
+int Div(int x, int y) {
+	return x / y;
+}
+
+int Exp(int x, int y) {
+	return pow(x, y);
+}
+
+int XOR(int x, int y) {
+	return x ^ y;
+}
+
+//繁杂冗余的方法：
+//int main() {
+//	int input = 0;
+//	int x = 0;
+//	int y = 0;
+//	do {
+//		menu();
+//		printf("请选择：>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Add(x, y));
+//			break;
+//		case 2:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Sub(x, y));
+//			break;
+//		case 3:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Mul(x, y));
+//			break;
+//		case 4:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Div(x, y));
+//			break;
+//		case 0:
+//			printf("退出！\n");
+//			break;
+//		default:
+//			printf("输入有误，请重新输入！！！\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
+
+int main() {
+	int input = 0;
+	int x = 0;
+	int y = 0;
+	int(*pfArr[7])(int, int) = { 0,Add,Sub,Mul,Div,Exp,XOR };//函数指针数组
+	do {
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		if (input >= 1 && input <= 6) {
+			printf("请输入操作数：>");
+			scanf("%d%d", &x, &y);
+			int ret = pfArr[input](x, y);
+			printf("%d\n", ret);
+		}
+		else if (input == 0) {
+			printf("退出！\n");
+		}
+		else {
+			printf("输入有误！！！\n");
+		}
+	} while (input);
+
+	return 0;
+}
